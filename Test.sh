6ce30e5b13e550b6dccf5ff879afd4d9768ba99d
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-29 19:37:19 +0530

    mongodb script

--- Test.sh
+++ Test.sh
@@ -2,7 +2,11 @@
 
 sudo mkdir -p /var/log/mongodb
 LOG_DIR=/var/log/mongodb
+echo " $? Directory created succesfully"
 sudo chown -R ubuntu:ubuntu $LOG_DIR
 sudo chmod -R 755 $LOG_DIR
+echo " $? Permissions succesfully"
+echo $0
 SCRIPT_NAME=$(basename "$0")
+echo "$SCRIPT_NAME"
 LOGFILE="$LOG_DIR/$SCRIPT_NAME.log"
