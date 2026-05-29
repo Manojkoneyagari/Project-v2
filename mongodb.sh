17d1de1dd3fb86d1e271c72e215095c4355e07a2
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-29 18:59:13 +0530

    mongodb script

---
+++ mongodb.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+LOG_DIR=$(sudo mkdir -p /var/log/mongodb)
+sudo chown -R ubuntu:ubuntu $LOG_DIR
+sudo chmod -R 755 $LOG_DIR
+LOGFILE="$LOG_DIR/$0.log"
+
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+    if [ $USERID -ne 0 ]; then
+        echo " Please run the script with root user or sudo access" | tee -a $LOGFILE
+    fi
+
+
+    Validate(){
+        if [ $1 -eq 0 ]; then
+            echo -e "$Timestamp [ Info ] $2 ... ${G} success ${N}" | tee -a $LOGFILE
+        else
+            echo -e "$Timestamp [ Error ] $2 ... ${R} failed ${N}" | tee -a $LOGFILE
+        fi
+    }
+
+
+
+
+#Import key
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+--dearmor &>> $LOGFILE
+Validate $? "Adding mongodb gpg keys"
+
+#adding repo
+echo "deb [ arch=amd64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | \
+sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list &>> $LOGFILE
+Validate $? "Adding repository"
+
+sudo apt update -y
+sudo apt install -y mongodb-org &>> $LOGFILE
+Validate $? "Updating and installing mongodb application"
+
+sudo systemctl enable mongod
+sudo systemctl start mongod
+Validate $? "Starting and enabling the mongodb service"
+
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+Validate $? "Changes in systemdfile to allow all traffic"
+
+sudo systemctl restart mongod
+Validate $? "Restarting systemd service"
