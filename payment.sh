75c9758cb08f33202eeaafc179f999af93537e08
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-30 12:41:23 +0530

    mongodb script

---
+++ payment.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+sudo mkdir -p /var/log/roboshop
+LOG_DIR=/var/log/roboshop
+sudo chown -R ubuntu:ubuntu $LOG_DIR
+sudo chmod -R 755 $LOG_DIR
+SCRIPT_NAME=$(basename "$0")
+echo -e "${Y}....$SCRIPT_NAME ${N}"
+LOGFILE="$LOG_DIR/${SCRIPT_NAME}.log"
+SCRIPT_DIR=$PWD
+appname=payment
+
+USERID=$(id -u)
+Timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+    if [ $USERID -ne 0 ]; then
+        echo " Please run the script with root user or sudo access" | tee -a $LOGFILE
+        exit 1;
+    fi
+
+
+    Validate(){
+        if [ $1 -eq 0 ]; then
+            echo -e "$Timestamp [ Info ] $2 ... ${G} success ${N}" | tee -a $LOGFILE
+        else
+            echo -e "$Timestamp [ Error ] $2 ... ${R} failed ${N}" | tee -a $LOGFILE
+        fi
+    }
+
+
+    id roboshop &>> $LOGFILE
+    if [ $? -eq 0 ]; then
+       echo " roboshop user already created "
+    else
+       echo " Creating roboshop user "
+      sudo useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+      Validate $? "Adding System user roboshop"
+    fi
+
+
+
+sudo apt update -y $LOG_DIR
+sudo apt install unzip -y
+Validate $? "Installing updates and unzip" 
+
+sudo apt install python3 gcc python3-dev -y $LOG_DIR
+Validate $? "Installing python"
+
+
+sudo apt install python3-pip -y $LOG_DIR
+Validate $? "Installing pip3"
+
+ cd /app
+        if [ $? -eq 0 ]; then
+            echo " Directory already exist, so removing the old code"
+            sudo rm -rf /app
+            Validate $? "Removed Directory"
+        fi
+
+
+
+echo " Directory not exists, creating directory and downloading code"
+sudo mkdir -p /app
+sudo curl -L -o /tmp/$appname.zip https://roboshop-artifacts.s3.amazonaws.com/$appname-v3.zip 
+cd /app 
+unzip /tmp/$appname.zip
+Validate $? "Downloading code"
+
+sudo pip3 install -r requirements.txt $LOG_DIR
+Validate $? "Installing dependencies"
+
+cp $SCRIPT_DIR/$appname.service /etc/systemd/system/$appname.service
+Validate $? "Creating Systemctl service"
+
+sudo systemctl daemon-reload
+sudo systemctl enable $appname 
+sudo systemctl start $appname
+Validate $? "Creating enable and start service"
+
